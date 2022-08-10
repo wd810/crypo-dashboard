d60ecc44e7fe2c93dba7a707aba96641b307d0fd
wangdian <36665764+wangdianyh@users.noreply.github.com>
2022-08-10 17:48:13 -0300

    install router and lab

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { BrowserRouter } from "react-router-dom"
 import './App.css';
+import Header from "./Components/Header";
 
 function App() {
   return (
-    <div className="App">
-      hello world!
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header/>
+      </div>
+    </BrowserRouter>
   );
 }
 
